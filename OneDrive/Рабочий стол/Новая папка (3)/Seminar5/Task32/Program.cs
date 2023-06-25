e866f6134759f8ecbee41e26824635e138e9a930
AlanZagalaev <manofword@mail.ru>
2023-06-25 17:36:33 +0300

    домашка по семинару 5

---
+++ OneDrive/Рабочий стол/Новая папка (3)/Seminar5/Task32/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 32: Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие 
+// отрицательные, и наоборот.
+
+int [] GetRandomArray()
+{
+    int[] array = new int[4];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(-100, 100);
+    }
+    return array;
+}
+
+int[] array = GetRandomArray();
+Console.WriteLine("Начальный массив ");
+Console.WriteLine(String.Join(" , ", array));
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = -array[i];  
+}
+Console.WriteLine("Измененный массив ");
+Console.WriteLine(String.Join(" , ", array));
+
---
+++ OneDrive/Рабочий стол/Новая папка (3)/Seminar5/Task38/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между 
+// максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+double[] numbers = {3.22, 4.2, 1.15, 77.15, 65.2};
+
+// double diff = 0;
+double max = numbers[0];
+
+
+
+double min = numbers[0];
+
+for(int d = 0; d < numbers.Length; d++)
+    {
+        if(numbers[d] > max) 
+        {
+            max = numbers[d];
+        }
+        if(numbers[d] < min)
+        {
+            min = numbers[d];
+        }
+        
+    }
+double diff = max - min;
+Console.WriteLine($"{max} - {min} = {diff}");
