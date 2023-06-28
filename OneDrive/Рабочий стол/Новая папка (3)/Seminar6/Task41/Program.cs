b3732f3a2a7f5eee6eabcc917e716e9d99e0e247
AlanZagalaev <manofword@mail.ru>
2023-06-28 18:52:04 +0300

    homework 6

---
+++ OneDrive/Рабочий стол/Новая папка (3)/Seminar6/Task41/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+
+Console.WriteLine("Введите количество чисел M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+for(int i = 0; i < M; i++)
+{
+    Console.WriteLine("Vvedi 4islo: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if(number > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine("Koli4estvo positive 4isel: " + count);
