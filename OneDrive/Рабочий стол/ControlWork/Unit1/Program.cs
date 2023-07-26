f9792b557c56ac220e870383de848793c3653603
AlanZagalaev <manofword@mail.ru>
2023-07-27 01:11:34 +0300

    результат сохраняеv в массиве inputArray.

--- OneDrive/Рабочий стол/ControlWork/Unit1/Program.cs
+++ OneDrive/Рабочий стол/ControlWork/Unit1/Program.cs
@@ -3,3 +3,6 @@
     static void Main(string[] args)
     {
         Console.WriteLine("Введите элементы массива (разделите их пробелом):");
+        string input = Console.ReadLine();
+        string[] inputArray = input.Split(' '); 
+        string[] resultArray = FilterArray(inputArray);
