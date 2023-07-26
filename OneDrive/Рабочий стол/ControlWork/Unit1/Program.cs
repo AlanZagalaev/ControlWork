d0d7a3671f78d03a9e2b0da40f778c63b1338f59
AlanZagalaev <manofword@mail.ru>
2023-07-27 01:34:46 +0300

    перебираем элементы массива array

--- OneDrive/Рабочий стол/ControlWork/Unit1/Program.cs
+++ OneDrive/Рабочий стол/ControlWork/Unit1/Program.cs
@@ -26,3 +26,11 @@ static string[] FilterArray(string[] array)
         }
         string[] filteredArray = new string[newLength];
         int index = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= 3)
+            {
+                filteredArray[index] = array[i];
+                index++;
+            }
+        }
