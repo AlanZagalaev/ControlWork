92f4f6babf5b4045c765b8d485f6ca8579647be3
AlanZagalaev <manofword@mail.ru>
2023-07-27 01:27:25 +0300

    перебираем через цикл  элементы массива array

--- OneDrive/Рабочий стол/ControlWork/Unit1/Program.cs
+++ OneDrive/Рабочий стол/ControlWork/Unit1/Program.cs
@@ -17,3 +17,10 @@
 static string[] FilterArray(string[] array)
     {   
         int newLength = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= 3)
+            {
+                newLength++;
+            }
+        }
